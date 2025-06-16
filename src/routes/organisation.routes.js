6e1b744d648c9d70e4ce9a615806cfef71c16a87
Geoffreyriv974 <geoffrey.riviere@ecole-it.com>
2025-06-16 10:51:34 +0200

    feat: ajout de la modification d'une organisation

--- src/routes/organisation.routes.js
+++ src/routes/organisation.routes.js
@@ -2,6 +2,8 @@ import {response, Router} from "express"
 import { Organisation } from "../models/organisation.model.js";
 import { User } from "../models/user.model.js";
 import {userRouter} from "./user.routes.js";
+import {body, validationResult} from "express-validator";
+import {DataTypes, Op, or} from "sequelize";
 
 export const organisationRouter = Router()
 
@@ -34,6 +36,35 @@ organisationRouter.get('/:id', async function(req, res){
         return res.status(404).json({message: "l'organisation n'éxiste pas !"})
     }
     res.status(200).json(organisation)
+})
+
+organisationRouter.put('/:id', async function (req, res){
+    const organisation = await Organisation.findByPk(req.params.id);
+    if (organisation == null){
+        return res.status(404).json({message: "L'organisation n'éxiste pas !"})
+    }
+
+    try {
+
+        if (await Organisation.findOne({where: {name: req.body.name, id: {[Op.ne]: req.params.id}}})) {
+            res.status(409).json({ message: "Le nom est déja utilisé"})
+            return
+        }
+
+        organisation.name = req.body.name
+        organisation.description = req.body.description
+        organisation.country = req.body.country
+        organisation.address = req.body.address
+        organisation.isAuthorized = req.body.isAuthorized
+        organisation.logo = req.body.logo
+
+        await organisation.save()
+        await organisation.reload()
+
+        res.status(200).json(organisation)
+    }catch (err) {
+        res.status(500).json({ message: "Erreur lors de la modification de l'organisation !", err});
+    }
 })
 
 organisationRouter.delete('/:id', async function (req, res){
