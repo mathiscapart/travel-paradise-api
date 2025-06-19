7782a18b073ccb22352cd393ae58096180efff5d
Geoffreyriv974 <geoffrey.riviere@ecole-it.com>
2025-06-20 00:22:32 +0200

    feat: Ajout de la route PATCH pour les visites

--- src/routes/visite.routes.js
+++ src/routes/visite.routes.js
@@ -2,6 +2,10 @@ import {Router} from "express"
 import { Visite } from "../models/visite.model.js";
 import {User} from "../models/user.model.js";
 import {userRouter} from "./user.routes.js";
+import {body, validationResult} from "express-validator";
+import {Op} from "sequelize";
+import {listeEquipement} from "../models/listeEquipement.model.js";
+import {organisationRouter} from "./organisation.routes.js";
 
 export const visiteRouter = Router()
 
@@ -13,6 +17,30 @@ visiteRouter.post('/', async function (req, res){
     }catch (err) {
         console.error(err)
         res.status(500).json({ message: "Erreur lors de la création de la visite !", err});
+    }
+})
+
+visiteRouter.patch('/:id', async function (req, res){
+    const visite = await Visite.findByPk(req.params.id);
+    const Equipement = await listeEquipement.findByPk(req.params.id);
+    if (Equipement == null){
+        return res.status(404).json({message: "L'équipement n'éxiste pas !"})
+    }
+
+    try {
+        visite.name = req.body.name
+        visite.image = req.body.image
+        visite.country = req.body.country
+        visite.address = req.body.address
+        visite.town = req.body.town
+        visite.price = req.body.price
+        visite.ListeEquipements = req.body.ListeEquipement
+        visite.duration = req.body.duration
+        await visite.save()
+        await visite.reload()
+        res.status(200).json({ message: "La visite a bien était modifié !"} )
+    }catch (err) {
+        res.status(500).json({ message: "Erreur lors de la modification de la visite !", err});
     }
 })
 
